cdbcc15bc7737dc1a8a1eaf3209677449cded57e
Naki <nbsp1221@gmail.com>
2023-04-05 21:19:20 +0900

    🚑 Fix compatibility issue between ES module and CommonJS module

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,14 +5,16 @@ export default {
   input: 'src/index.ts',
   output: [
     {
-      dir: 'dist/cjs',
+      dir: 'dist',
       format: 'cjs',
+      entryFileNames: '[name].cjs',
       preserveModules: true,
       sourcemap: true,
     },
     {
-      dir: 'dist/esm',
+      dir: 'dist',
       format: 'esm',
+      entryFileNames: '[name].js',
       preserveModules: true,
       sourcemap: true,
     },
